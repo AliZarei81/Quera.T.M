8f944957e3912dcbb68ef1396c5056dfb6a5e380
Mohammad Bakhshi <77542557+mohammad-bakhshi@users.noreply.github.com>
2023-09-18 23:13:59 +0430

    add components folder to the project folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import Button from "./component/Button";
 function App() {
   return (
     <div className="App">
       <h1 className="text-body-xl font-bold m-l underline text-grape-primary font-iran-yekan">
         فونت ایران‌یکان
       </h1>
+      {/* <Button
+        onClick={() => console.log("test")}
+        title="test"
+        className="bg-red-primary w-l h-l"
+      /> */}
     </div>
   );
 }
