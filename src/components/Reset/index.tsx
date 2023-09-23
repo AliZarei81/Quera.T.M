9d04991ced2dfb14bc1e61ebdc0a919a9f26636c
Amirzr21 <amirzaree701@gmail.com>
2023-09-23 19:20:56 +0330

    fix reset

--- src/components/Reset/index.tsx
+++ src/components/Reset/index.tsx
@@ -1,10 +1,11 @@
 import { useState, FormEvent, ChangeEvent } from "react";
 import Button from "../Button";
 import Input from "../Input";
+import Form from "../Form";
 
 const ResetPassword: React.FC = () => {
-  const [password, setPassword] = useState<string>(""); 
-  const [isFormSubmitted, setIsFormSubmitted] = useState<boolean>(false);
+  const [password, setPassword] = useState<string>("");
+  const [, setIsFormSubmitted] = useState<boolean>(false);
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
@@ -17,28 +18,25 @@ const ResetPassword: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center w-[640px] gap-[32px] p-[24px] bg-white shadow-2xl rounded-b-2xl">
+    <div className="flex flex-col items-center justify-center w-[640px] gap-[32px] p-[24px]  [background-color:#ffff] shadow-2xl rounded-b-2xl">
       <h3 className="font-iran-yekan w-[237px] h-[55px] text-[32px] font-black text-center">
-      بازیابی رمز عبور 
+        بازیابی رمز عبور
       </h3>
-    
-          <form
-            onSubmit={handleSubmit}
-            className="flex flex-col items-center justify-center w-[592] h-[140px] gap-[32px]"
-          >
-            <Input
-              type="password"
-              label="رمز عبور جدید را وارد کنید"
-              onChange={handlePasswordChange}
-              value={password}
-            />
-            <Button
-              type="submit"
-              disabled={!password}
-              className="font-iran-yekan w-148 h-12 px-3 py-3 p-[10px] gap-8 text-lg font-bold text-center text-white bg-brand-primary text-gray-secondary rounded cursor-pointer"
-              title="تغییر رمز عبور"
-            />
-          </form>  
+
+      <Form onSubmit={handleSubmit}>
+        <Input
+          type="password"
+          label="رمز عبور جدید را وارد کنید"
+          onChange={handlePasswordChange}
+          value={password}
+        />
+        <Button
+          type="submit"
+          disabled={!password}
+          className="font-iran-yekan w-148 h-12 px-3 py-3 p-[10px] gap-8 text-lg font-bold text-center text-white bg-brand-primary text-gray-secondary rounded cursor-pointer"
+          title="تغییر رمز عبور"
+        />
+      </Form>
     </div>
   );
 };
