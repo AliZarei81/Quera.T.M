e2490a4ed202541d8f7c6291db7d34f67ecb3044
Amirzr21 <amirzaree701@gmail.com>
2023-09-23 19:16:27 +0330

    test components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+// import ForgetPassword from "./components/Forgot";
+// import ResetPassword from "./components/Reset";
+// import Login from "./components/Login";
+// import Register from "./components/Register";
+import Design from "./components/Design";
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Design></Design>
+    {/* <Register></Register> */}
+    {/* <Login></Login> */}
+    {/* <ForgetPassword></ForgetPassword> */}
+    {/* <ResetPassword></ResetPassword> */}
+  </div>;
 }
 
 export default App;
