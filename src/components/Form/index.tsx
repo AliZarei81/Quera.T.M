b6bfb34b6b25d36e86b2827f1f1b57e0ad7a636d
Amirzr21 <amirzaree701@gmail.com>
2023-09-23 19:18:48 +0330

    fixed form

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,19 @@
+import { FormEvent, ReactNode } from "react";
+
+interface IFormProps {
+  children: ReactNode;
+  onSubmit?: (event: FormEvent<HTMLFormElement>) => void;
+}
+
+const Form: React.FC<IFormProps> = ({ children, onSubmit }) => {
+  return (
+    <form
+      onSubmit={onSubmit}
+      className="flex flex-col items-center justify-center w-[592] gap-[20px]"
+    >
+      {children}
+    </form>
+  );
+};
+
+export default Form;
