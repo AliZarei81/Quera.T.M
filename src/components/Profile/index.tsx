b795bbd0e6f91a47ae8effed9981a6c8a8e3c2aa
Zahra <zahra.reh1998@gmal.com>
2023-09-26 00:18:47 +0330

    - Added profileColor prop to customize profile background color. - Added conditional rendering of "Workspace Owner" tag for owners.

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -3,19 +3,25 @@ interface IProfileProbs {
   userName: string;
   userProfilePicture?: string;
   hasProfilePicture: boolean;
+  isOwner: boolean;
+  profileColor: string;
 }
 
 const Profile: React.FC<IProfileProbs> = ({
   userName,
   userProfilePicture,
   hasProfilePicture,
+  isOwner = false,
+  profileColor,
 }): JSX.Element => {
   // Extract initials from the user's name
+
   const initials = userName
     .split(" ")
     .map((namePart) => namePart.charAt(0))
     .join("");
   // Function to load the profile picture or show initials
+
   const renderProfilePicture = () => {
     if (hasProfilePicture) {
       return (
@@ -27,7 +33,9 @@ const Profile: React.FC<IProfileProbs> = ({
       );
     } else {
       return (
-        <div className="w-[36px] h-[37px] rounded-full bg-indigo-secondary flex items-center justify-center">
+        <div
+          className={` w-[36px] h-[37px] rounded-full bg-indigo-secondary flex items-center justify-center ${profileColor}`}
+        >
           <span className="text-xs font-normal text-indigo-primary ">
             {initials}
           </span>
@@ -35,11 +43,22 @@ const Profile: React.FC<IProfileProbs> = ({
       );
     }
   };
-
+  //Funcion to set username
+  const user = () => {
+    if (isOwner) return "من";
+    else return userName;
+  };
   return (
-    <div className="flex h-[37px] gap-xs justify-start items-center ">
-      {renderProfilePicture()}
-      <h1 className="flex ">{userName}</h1>
+    <div className="flex justify-start w-[220px] gap-xs items-center">
+      <div className=" h-[37px] gap-xs justify-start items-center flex">
+        {renderProfilePicture()}
+        <h1 className="flex ">{user()}</h1>
+      </div>
+      {isOwner && (
+        <div className="rounded-md flex text-center justify-center items-center bg-blue-secondary text-blue-primary text-[12px] w-[114px] h-[29px] px-[8px] py-[4px] ">
+          Workspace Owner
+        </div>
+      )}
     </div>
   );
 };
