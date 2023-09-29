1b388fd9f3623923caf72cb922af221ae06f595d
Amirzr21 <amirzaree701@gmail.com>
2023-09-29 18:26:11 +0330

    navbar component

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import ListIcon from "../Icon/List";
-import ColumnIcon from "../Icon/Column";
-import CalenderIcon from "../Icon/Calender";
-import ShareIcon from "../Icon/Share";
+import React from "react";
+import Button from "../Button";
+import Link from "../Link";
+import { FiShare2 } from "react-icons/fi";
+import { BsCalendar4Week } from "react-icons/bs";
+import { BsKanban } from "react-icons/bs";
+import { LuListChecks } from "react-icons/lu";
 
 const Navbar: React.FC = () => {
   const [activeLink, setActiveLink] = React.useState<string | null>(null);
@@ -10,42 +12,62 @@ const Navbar: React.FC = () => {
   return (
     <div className="font-iran-yekan flex items-center justify-between py-[10px] border-b-2 border-gray-primary w-[1034px]">
       <div className="flex items-center justify-evenly self-center w-[511px] ">
-        <h3 className="text-[20px] font-extrabold pl-[10px]">
-          پروژه اول
-        </h3>
-        <div className={`flex items-center justify-center self-center gap-[5px] px-[20px]  border-l-2 border-r-2 border-gray-primary text-[16px] font-medium cursor-pointer hover:text-[rgba(32,141,142,1)] hover:font-extrabold   ${activeLink === 'list' ? 'before: opacity-100 border-b-2 border-[rgba(32,141,142,1)]' : ''}`}
-             onClick={() => setActiveLink('list')}>
-          <ListIcon className="hover:text-[rgba(32,141,142,1)]"></ListIcon>
-          <a className="text-black no-underline" href="#">
-            نمایش لیستی
-          </a>
+        <h3 className="text-[20px] font-extrabold pl-[10px]">پروژه اول</h3>
+        <div
+          className={`flex items-center justify-center self-center gap-[5px] px-[20px]  border-l-2 border-r-2 border-gray-primary text-[16px] font-medium cursor-pointer relative`}
+          onClick={() => setActiveLink("list")}
+        >
+          {activeLink === 'list' && (
+            <div className="absolute bottom-[-18px] left-0 w-full border-b-2 border-[rgba(32,141,142,1)] "></div>
+          )}
+          <Link
+            title="نمایش لیستی"
+            className={`no-underline gap-[5px] h-[16px] ${activeLink === 'list' ? 'text-[rgba(32,141,142,1)]' : 'text-black'} hover:text-[rgba(32,141,142,1)]`}
+            href="#"
+            icon={<LuListChecks />}
+          ></Link>
         </div>
-        <div className={`flex items-center justify-center self-center gap-[5px] px-[20px]  border-l-2 border-gray-primary text-[16px] font-medium cursor-pointer hover:text-[rgba(32,141,142,1)] hover:font-extrabold ${activeLink === 'column' ? 'border-b-2 border-[rgba(32,141,142,1)]' : ''}`}
-             onClick={() => setActiveLink('column')}>
-          <ColumnIcon className="hover:text-[rgba(32,141,142,1)]"></ColumnIcon>
-          <a className="text-black no-underline" href="#">
-            نمایش ستونی
-          </a>
+        <div
+          className={`flex items-center justify-center self-center gap-[5px] px-[20px]  border-l-2  border-gray-primary text-[16px] font-medium cursor-pointer relative`}
+          onClick={() => setActiveLink("column")}
+        >
+          {activeLink === 'column' && (
+            <div className="absolute bottom-[-18px] left-0 w-full border-b-2 border-[rgba(32,141,142,1)]"></div>
+          )}
+          <Link
+            title="نمایش ستونی"
+            className={`no-underline gap-[5px] h-[16px] ${activeLink === 'column' ? 'text-[rgba(32,141,142,1)]' : 'text-black'} hover:text-[rgba(32,141,142,1)]`}
+            href="#"
+            icon={<BsKanban />}
+          ></Link>
         </div>
-        <div className={`flex items-center justify-center self-center gap-[5px] px-[20px] border-l-2 border-gray-primary text-[16px] font-medium cursor-pointer hover:text-[rgba(32,141,142,1)] hover:font-extrabold ${activeLink === 'calendar' ? 'border-b-2 border-[rgba(32,141,142,1)] ' : ''}`}
-             onClick={() => setActiveLink('calendar')}>
-          <CalenderIcon className="hover:text-[rgba(32,141,142,1)]"></CalenderIcon>
-          <a className="text-black no-underline" href="#">
-            تقویم
-          </a>
+        <div
+          className={`flex items-center justify-center self-center px-[20px] border-l-2 border-gray-primary text-[16px] font-medium cursor-pointer relative`}
+          onClick={() => setActiveLink("calendar")}
+        >
+          {activeLink === 'calendar' && (
+            <div className="absolute bottom-[-18px] left-0 w-full border-b-2 border-[rgba(32,141,142,1)]"></div>
+          )}
+          <Link
+            title="تقویم"
+            className={`no-underline gap-[5px] h-[16px] ${activeLink === 'calendar' ? 'text-[rgba(32,141,142,1)]' : 'text-black'} hover:text-[rgba(32,141,142,1)]`}
+            href="#"
+            icon={<BsCalendar4Week />}
+          ></Link>
         </div>
       </div>
 
-      <div className="flex items-center justify-center gap-[3px]">
-        <ShareIcon></ShareIcon>
-        <button className="p-2 text-black bg-white rounded-full border-none cursor-pointer font-medium text-[16px] ">
-          اشتراک‌گذاری
-        </button>
+      <div className="flex items-center justify-center gap-[5px]">
+        <Button
+          className=" font-iran-yekan p-2 text-black bg-white rounded-full border-none cursor-pointer font-medium text-[16px] "
+          title="اشتراک‌گذاری"
+          disabled={false}
+          icon={<FiShare2 />}
+        />
       </div>
     </div>
   );
 };
 
 export default Navbar;
-
 
