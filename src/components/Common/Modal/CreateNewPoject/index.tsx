c48079226cc3d2dbecb15a9d7b27cd4dcfdd9d9c
Zahra <zahra.reh1998@gmal.com>
2023-10-02 12:26:04 +0330

    -create modal component

---
+++ src/components/Common/Modal/CreateNewPoject/index.tsx
@@ -0,0 +1,31 @@
+import { ChangeEvent, useState } from "react";
+import Modal from "..";
+import Input from "../../Input";
+interface ICreatNewProjectProbs{
+    handleSubmit:()=>void;
+}
+const CreateNewProject: React.FC<ICreatNewProjectProbs> = ({handleSubmit}): JSX.Element => {
+    const[projectName,setProjectName]=useState<string>('')
+    const handleProjectNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setProjectName(event.target.value);
+      };
+      console.log(projectName)
+  const child = <Input type="text" id="projectName" value={projectName} label={{text:'نام پروژه' , for:'projectName'}} onChange={handleProjectNameChange} className="w-[415px]" />;
+  
+  
+  
+  return (
+    <Modal
+      buttonTitle="ادامه"
+      currentPage={1}
+      mBody={child}
+      totalPages={1}
+      modalTitle="ساختن پروژه جدید"
+      hasPaginationBulet={false}
+      modalClassname="w-[500px] h-[272px] "
+      onClick={handleSubmit}
+    />
+  );
+};
+
+export default CreateNewProject;
---
+++ src/components/Common/Modal/ShareProject/index.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import {AiOutlineLink} from "react-icons/ai"
+const ShareProject :React.FC=():JSX.Element=>{
+    return(
+        <></>
+    )
+}
+export default ShareProject
